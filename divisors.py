1c21af1d383ca7a57c0f87d82dbbc1da8b905e0b
Eoin OKennedy <Eoin.okennedy@tudublin.ie>
2022-02-06 17:21:52 +0000

    removed code

--- divisors.py
+++ divisors.py
@@ -2,20 +2,15 @@
 #and returns a list of all the divisors(factors) of that that number -
 #A divisor or factor is a number which divides evenly leaving no remainder
 #define the funciton header called divisors expecting one argument
-#the 
-def divisors(number):
+
     #set up an empty list to hold your result
-    divisorList = []
-    #loop i from 1 to the number you are checking
-    for i in range(1,number):
-        #if the remainder when dividing the number by i is equal to zero - i is a divisor so...
-        if (number%i==0):
+    
+    #loop i from 1 to the number you are checking (take care not to include the number itself)
+
+        #if the remainder when dividing the number by i is equal to zero then i is a divisor so...
+        
             #apend i to the list you set up
-            divisorList.append(i)
-       
+            
  
     #return the list
-    return divisorList
     
-
-
--- perfectNumber.py
+++ perfectNumber.py
@@ -3,12 +3,11 @@
 #write a function below called perfectNumber(x) which checks to see if x is a perfect number
 from divisors import divisors
 #define the function header called perfectNumber expecting one argument
-def perfectNumber(number):
+
     #set a result variable to False by default
-    result = False
+    
     #if the sum of all the divisors of the number is equal to the test number
-    if (sum(divisors(number))==number):
+
         #set the result variable to be True
-        result = True
+
     #return the result variable
-    return result
