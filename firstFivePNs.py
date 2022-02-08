ca84be56b51718d39b6337a62c457469787d6d52
Eoin OKennedy <Eoin.okennedy@tudublin.ie>
2022-02-08 14:16:00 +0000

    added final step to find the first five PNS

---
+++ firstFivePNs.py
@@ -0,0 +1,12 @@
+#run this program when you have completed the first two parts of the exercise
+#if you have completed the first two parts correctly this will find the first five perfect nubmers
+
+from perfectNumber import perfectNumber
+
+totalPFs = 0
+testNumber = 1
+while (totalPFs <5):
+    if (perfectNumber(testNumber)):
+        print(testNumber," is a perfect number")
+        totalPFs = totalPFs + 1
+        testNumber = testNumber + 1
