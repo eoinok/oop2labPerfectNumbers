afe956614ff8d1c36fb9c31fcc233d4ea91a2348
Eoin OKennedy <Eoin.okennedy@tudublin.ie>
2024-02-06 17:07:33 +0000

    changed firstFive to firstFour

--- findFirstFourPNs.py
+++ findFirstFourPNs.py
@@ -1,11 +1,11 @@
 #run this program when you have completed the first two parts of the exercise
-#if you have completed the first two parts correctly this will find the first five perfect nubmers
+#if you have completed the first two parts correctly this will find the first four perfect nubmers
 
 from perfectNumber import perfectNumber
 
 totalPFs = 0
 testNumber = 1
-while (totalPFs <5):
+while (totalPFs <= 4):
     if (perfectNumber(testNumber)):
         print(testNumber," is a perfect number")
         totalPFs = totalPFs + 1
---
+++ test_vehicle.py
@@ -0,0 +1,9 @@
+#test_vehicle.py
+import pytest
+from Vehicle import Vehicle
+
+def test_vehicle():
+    myVehicle = Vehicle("00D1122","Ford","Mondeo")
+    myVehicle.set_miles(10000)
+    assert myVehicle.getMakeModel() == "Ford Mondeo"
+
